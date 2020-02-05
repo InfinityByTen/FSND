02406fc377dd0c0cb92c5993041ede8b364991d8
Aseem Dua <8684135+InfinityByTen@users.noreply.github.com>
2020-02-05 21:31:34 +0100

    Migrate to a complete model for Venue, Artist and Show
    Relationships between the models established and columns updated
    to be pedantic on non-nullable columns from the form. Added a repr
    method to artist and venue to help with visualising on the interactive
    CLI.

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -32,29 +32,41 @@ class Venue(db.Model):
     __tablename__ = 'Venue'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, nullable=False, default=False)
+    # Self TODO: see if description state should be dependent on the bool value
+    seeking_description = db.Column(db.String(240))
+    genres = db.Column(db.ARRAY(db.String))
+    show_ref = db.relationship('Show', backref='venue',
+      lazy = True)
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    def __repr__(self):
+      return f'<Venue {self.id}: "{self.name}", in ({self.city}, {self.state})>'
+
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    show_ref = db.relationship('Show', backref='artist',
+      lazy = True)
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    def __repr__(self):
+      return f'<Artist {self.id}: "{self.name}", from ({self.city}, {self.state})>'
 
 class Show(db.Model):
   __tablename__ = 'Show'
@@ -62,6 +74,10 @@ class Show(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   start_time = db.Column(db.DateTime, nullable=False,
         default=datetime.utcnow)
+  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'),
+    nullable=False)
+  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'),
+    nullable=False)
 
 #----------------------------------------------------------------------------#
 # Filters.
